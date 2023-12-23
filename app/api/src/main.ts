5ff08f4d40e3a2faf6549803125e9092218e728a
Mohamedhadhoud1 <muhammed195837@feng.bu.edu.eg>
2023-12-24 01:49:42 +0200

    .

--- app/api/src/main.ts
+++ app/api/src/main.ts
@@ -1,19 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-//import * as cookieParser from 'cookie-parser';
-//import * as dotenv from 'dotenv';
+import * as cookieParser from 'cookie-parser';
+import * as dotenv from 'dotenv';
 
-//dotenv.config();
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //const port = process.env.PORT || 3000;
-  //app.use(cookieParser() as any);
+  const port = process.env.PORT || 3000;
+  app.use(cookieParser());
   app.enableCors({
     origin: true,
     credentials: true,
     allowedHeaders: ['Content-Type'],
   });
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
