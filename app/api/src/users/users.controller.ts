c31c4dffed99b01123d3e06354107bf80d5f014f
Mohamedhadhoud1 <muhammed195837@feng.bu.edu.eg>
2023-12-26 07:10:56 +0200

    jwt

--- app/api/src/users/users.controller.ts
+++ app/api/src/users/users.controller.ts
@@ -58,7 +58,7 @@ export class UsersController {
       const jwt = await this.jwtService.signAsync({ id: user.id });
       console.log('hhe', jwt);
       response.cookie('jwt', jwt, {
-        secure: false,
+        secure: true,
         httpOnly: true,
         sameSite: 'none',
         path: '/',
